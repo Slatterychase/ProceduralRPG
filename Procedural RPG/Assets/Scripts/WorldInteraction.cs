8f3a956b3f43e436c52f7aed321ccac4f4a06857
Chase Slattery <chs6298@rit.edu>
2018-01-05 18:04:26 -0500

    Added click to move functionality

---
+++ Procedural RPG/Assets/Scripts/WorldInteraction.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WorldInteraction : MonoBehaviour {
+   NavMeshAgent playerAgent;
+    void Start()
+    {
+        playerAgent = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject())
+        {
+            GetInteraction();
+        }
+    }
+    void GetInteraction()
+    {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if(Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if(interactedObject.tag == "Interactable")
+            {
+                Debug.Log("It is interactable");
+            }
+            else
+            {
+                playerAgent.destination = interactionInfo.point;
+            }
+        }
+    }
+}
