a0c6c1c3f338e2b9367f05e5395c63320c0e19a0
Chase Slattery <chs6298@rit.edu>
2018-01-15 16:36:16 -0500

    Added basic dialogue creation with ability for slogans and nicknames

---
+++ Procedural RPG/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneManager : MonoBehaviour {
+    public List<string> firstDialogue;
+    public List<string> secondDialogue;
+    public List<string> thirdDialogue;
+    public string slogan;
+    public string playerTempName;
+    public bool hasNickname = true;
+    public string nickname;
+	// Use this for initialization
+	void Start ()
+    {
+        if(hasNickname == true)
+        {
+            playerTempName = nickname;
+        }
+        for (int x = 0; x < firstDialogue.Count; x++)
+        {
+            firstDialogue[x] = slogan + " " + playerTempName + " " + firstDialogue[x];
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
